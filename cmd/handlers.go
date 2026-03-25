3270566b2b3a32292b7df77f48424f164f316c0a
Vicky Kumar <ce21btech11045@iith.ac.in>
2026-03-25 18:00:38 +0900

    fix : expose user handler

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -4,10 +4,12 @@ import "pizza-tracking/internal/models"
 
 type Handler struct {
 	orders *models.OrderModel
+	users  *models.UserModel
 }
 
 func NewHandler(dbModel *models.DBModel) *Handler {
 	return &Handler{
 		orders: &dbModel.Order,
+		users:  &dbModel.Users,
 	}
 }
