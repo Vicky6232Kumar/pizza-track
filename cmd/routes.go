b8265d8121b708e59c9d315e523bdad4fc3a1b7f
Vicky Kumar <ce21btech11045@iith.ac.in>
2026-03-26 22:55:50 +0900

    feat : routes for the notification event

--- cmd/routes.go
+++ cmd/routes.go
@@ -17,6 +17,7 @@ func setUpRoutes(router *gin.Engine, h *Handler, store sessions.Store) {
 		router.POST("/new-order", h.HandleNewOrderPost)
 		router.GET("/order/:id", h.HandleOrderGet)
 		router.POST("/logout", h.HandleUserLogout)
+		router.GET("/notifications", h.notificationHandler)
 	}
 
 	adminProtectedRoute := router.Group("/admin")
@@ -24,6 +25,7 @@ func setUpRoutes(router *gin.Engine, h *Handler, store sessions.Store) {
 	{
 		adminProtectedRoute.POST("/order/:id/update", h.HandleOrderDelete)
 		adminProtectedRoute.POST("/order/:id/delete", h.HandleOrderDelete)
+		adminProtectedRoute.GET("/notifications", h.adminNotificationHandler)
 	}
 
 }
